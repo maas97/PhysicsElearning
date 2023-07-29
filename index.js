c11c981a4d2ed2fd741ccebca57ba277b1c90314
maas97 <mashraf.14697@gmail.com>
2023-07-30 00:55:18 +0300

    modifying views path

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ app.use(
 );
 app.use(express.static('public'));
 app.use(morgan("combined"));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
