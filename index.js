96c9e21028e2370b47fa5de2fc98fe28e442356b
maas97 <mashraf.14697@gmail.com>
2023-08-12 04:28:39 +0300

    index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.use(
     })
 );
 app.use(morgan("combined"));
-app.set('views', path.join('/views'));
+app.set('views', path.join(__dirname + '/views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname + '/public')));
 
