ad800f72cbe57589ef6b8f374a9dd2bc85c46477
maas97 <mashraf.14697@gmail.com>
2023-08-27 19:27:42 +0300

    modify refresh controller

--- Middleware/refresh.js
+++ Middleware/refresh.js
@@ -1,7 +1,7 @@
 const { exec } = require("child_process");
 
 module.exports = (req, res) => {
-  exec("git pull origin main", (error) => {
+  exec("git pull origin master", (error) => {
     if (error) {
       console.error(`Error executing git pull: ${error.message}`);
       return res.status(500).send("Failed to execute git pull");
