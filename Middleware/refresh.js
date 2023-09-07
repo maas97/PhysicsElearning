b2bf6080ee09063d63e6e5186d46d402896a9ce0
maas97 <mashraf.14697@gmail.com>
2023-09-07 17:05:51 +0300

    minor change

--- Middleware/refresh.js
+++ Middleware/refresh.js
@@ -4,9 +4,9 @@ module.exports = (req, res) => {
   exec("git pull origin master", (error) => {
     if (error) {
       console.error(`Error executing git pull: ${error.message}`);
+      console.error(`Error executing git pull: ${error}`);
       return res.status(500).send("Failed to execute git pull");
     }
-
     console.log("Git pull successful");
     res.send("Git pull successful");
   });
